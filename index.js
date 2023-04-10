4633f85583e1554232411b70b2d8e5259caae065
Anil Patidar <patidaranil0791@gmail.com>
2023-04-10 12:05:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const fileUpload = require("express-fileupload");
 const pdfParse = require("pdf-parse");
+const cors = require("cors");
 
 const app = express();
 
+app.all("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type",'Authorization');
+  // res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+app.use(cors());
 app.use("/", express.static("public"));
 app.use(fileUpload());
 
